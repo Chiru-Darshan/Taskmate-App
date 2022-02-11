7b2246036df2eed91a761b6147a8963bd8acc66a
eq04978 <darshan.a@microfocus.com>
2022-02-11 20:20:52 +0530

    updated settings for gunicorn

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
@@ -136,4 +136,3 @@ LOGIN_URL='login'
 django_heroku.settings(locals())
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
